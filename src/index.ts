f69349d184519c4920010cb0f35c68b720bc8c3e
Nikita Malyschkin <nikita.malyschkin@googlemail.com>
2020-11-11 22:13:13 +0100

    replace plugin impl with config enchancer

--- src/index.ts
+++ src/index.ts
@@ -1,107 +1,101 @@
-import { Plugin, Compiler, BannerPlugin, DefinePlugin } from 'webpack';
-import { setEnvironment, getDefineVariables, getVariables } from './helpers';
-
-const pluginName = 'PiletWebpackPlugin';
+import { Plugin, Compiler, BannerPlugin, DefinePlugin } from "webpack";
+import { setEnvironment, getDefineVariables, getVariables } from "./helpers";
 
 export interface PiletWebpackPluginOptions {
-  /**
-   * The name of the pilet.
-   */
-  name: string;
-  /**
-   * The version of the pilet.
-   */
-  version: string;
-  /**
-   * The name of the Piral instance / app shell.
-   */
-  piral: string;
-  /**
-   * The schema version. By default, v1 is used.
-   */
-  schema?: 'v0' | 'v1' | 'none';
-  /**
-   * The shared dependencies. By default, these are read from the
-   * Piral instance.
-   */
-  externals?: Array<string>;
-  /**
-   * Additional environment variables to define.
-   */
-  variables?: Record<string, string>;
+    /**
+     * The name of the pilet.
+     */
+    name: string;
+    /**
+     * The version of the pilet.
+     */
+    version: string;
+    /**
+     * The name of the Piral instance / app shell.
+     */
+    piral: string;
+    /**
+     * The schema version. By default, v1 is used.
+     */
+    schema?: "v0" | "v1" | "none";
+    /**
+     * The shared dependencies. By default, these are read from the
+     * Piral instance.
+     */
+    externals?: Array<string>;
+    /**
+     * Additional environment variables to define.
+     */
+    variables?: Record<string, string>;
 }
 
 function getExternals(piral: string) {
-  const shellPkg = require(`${piral}/package.json`);
-  const piralExternals = shellPkg.pilets?.externals ?? [];
-  return [
-    ...piralExternals,
-    '@dbeining/react-atom',
-    '@libre/atom',
-    'history',
-    'react',
-    'react-dom',
-    'react-router',
-    'react-router-dom',
-    'tslib',
-    'path-to-regexp',
-  ];
+    const shellPkg = require(`${piral}/package.json`);
+    const piralExternals = shellPkg.pilets?.externals ?? [];
+    return [
+        ...piralExternals,
+        "@dbeining/react-atom",
+        "@libre/atom",
+        "history",
+        "react",
+        "react-dom",
+        "react-router",
+        "react-router-dom",
+        "tslib",
+        "path-to-regexp",
+    ];
 }
 
-export class PiletWebpackPlugin implements Plugin {
-  constructor(private options: PiletWebpackPluginOptions) { }
-
-  apply(compiler: Compiler) {
-    const environment = process.env.NODE_ENV || 'development';
-    const { name, version, piral, externals = getExternals(piral), schema } = this.options;
-    const shortName = name.replace(/\W/gi, '');
+export const piletWebpackConfigEnchancer = (options: PiletWebpackPluginOptions) => (compilerOptions) => {
+    const environment = process.env.NODE_ENV || "development";
+    const { name, version, piral, externals = getExternals(piral), schema } = options;
+    const shortName = name.replace(/\W/gi, "");
     const jsonpFunction = `pr_${shortName}`;
     const variables = {
-      ...getVariables(name, version, environment),
-      ...this.options.variables,
+        ...getVariables(name, version, environment),
+        ...options.variables,
     };
-    const plugins = [
-      new DefinePlugin(getDefineVariables(variables)),
-    ];
+    const plugins = [new DefinePlugin(getDefineVariables(variables))];
 
-    if (schema !== 'none') {
-      const bannerSuffix = schema ? `1(${jsonpFunction})` : `0`;
+    if (schema !== "none") {
+        const bannerSuffix = schema ? `1(${jsonpFunction})` : `0`;
 
-      plugins.push(
-        new BannerPlugin({
-          banner: `//@pilet v:${bannerSuffix}`,
-          entryOnly: true,
-          include: /\.js$/,
-          raw: true,
-        }),
-      );
+        plugins.push(
+            new BannerPlugin({
+                banner: `//@pilet v:${bannerSuffix}`,
+                entryOnly: true,
+                include: /\.js$/,
+                raw: true,
+            })
+        );
     }
 
+    // FIXME: this is a really bad idea, making finding the source of configuration nearly impossible
     setEnvironment(variables);
 
-    plugins.forEach(plugin => plugin.apply(compiler));
+    // plugins.forEach((plugin) => plugin.apply(compiler));
+    compilerOptions.plugins = [...compilerOptions.plugins, ...plugins];
 
-    compiler.hooks.afterEnvironment.tap(pluginName, () => {
-      const current = compiler.options.externals;
-      compiler.options.output.jsonpFunction = `${jsonpFunction}_chunks`;
-      compiler.options.output.library = name;
+    const current = compilerOptions.externals;
+    compilerOptions.output.jsonpFunction = `${jsonpFunction}_chunks`;
+    compilerOptions.output.library = name;
 
-      if (schema !== 'none') {
-        compiler.options.output.libraryTarget = 'umd';
-      }
+    if (schema !== "none") {
+        compilerOptions.output.libraryTarget = "umd";
+    }
 
-      if (schema === 'v1') {
-        const reset = environment !== 'production' ? `delete ${jsonpFunction}_chunks;` : '';
-        compiler.options.output.auxiliaryComment = {
-          commonjs2: `\nfunction define(d,k){${reset}(typeof document!=='undefined')&&(document.currentScript.app=k.apply(null,d.map(window.${jsonpFunction})));}define.amd=!0;`,
+    if (schema === "v1") {
+        const reset = environment !== "production" ? `delete ${jsonpFunction}_chunks;` : "";
+        compilerOptions.output.auxiliaryComment = {
+            commonjs2: `\nfunction define(d,k){${reset}(typeof document!=='undefined')&&(document.currentScript.app=k.apply(null,d.map(window.${jsonpFunction})));}define.amd=!0;`,
         } as any;
-      }
+    }
 
-      compiler.options.externals = Array.isArray(current)
+    compilerOptions.externals = Array.isArray(current)
         ? [...current, ...externals]
         : current
-          ? [current, ...externals]
-          : externals;
-    });
-  }
-}
+        ? [current, ...externals]
+        : externals;
+
+    return compilerOptions;
+};
